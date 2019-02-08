8e0b8f5aff2df3c1f3f1264e86dcf9c22a4dd20b
Kano Developer <opensource@kanoapps.com>
2019-02-08 13:12:33 -0800

    fixed missing includes

--- src/android/IdfaPlugin.java
+++ src/android/IdfaPlugin.java
@@ -1,27 +1,41 @@
 package by.chemerisuk.cordova.advertising;
 
 import android.content.Context;
-
-import by.chemerisuk.cordova.support.CordovaMethod;
-import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
+import android.util.Log;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 
-public class IdfaPlugin extends ReflectiveCordovaPlugin {
-    private static final String TAG = "IdfaPlugin";
+public class IdfaPlugin extends CordovaPlugin {
 
-    @CordovaMethod(ExecutionThread.WORKER)
-    protected void getInfo(CallbackContext callbackContext) throws Exception {
+    private static final String TAG = "idfaPlugin";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getInfo")) {
+            getInfo(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    protected void getInfo(final CallbackContext callbackContext)  {
         Context context = this.cordova.getActivity().getApplicationContext();
-        AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
-
         JSONObject result = new JSONObject();
-        result.put("aaid", info.getId());
-        result.put("limitAdTracking", info.isLimitAdTrackingEnabled());
+
+        try {
+            AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
+            result.put("aaid", info.getId());
+            result.put("limitAdTracking", info.isLimitAdTrackingEnabled());
+        } catch (Exception e) {
+            Log.e(TAG, "getInfo failed", e);
+        }
         callbackContext.success(result);
     }
 }
