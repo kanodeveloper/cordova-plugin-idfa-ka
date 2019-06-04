0461717eb073f958d55850261e21a399c8b6f5fc
Kano Developer <opensource@kanoapps.com>
2019-06-04 15:35:30 -0700

    added amazon device support

--- src/android/IdfaPlugin.java
+++ src/android/IdfaPlugin.java
@@ -2,13 +2,14 @@ package by.chemerisuk.cordova.advertising;
 
 import android.content.Context;
 import android.util.Log;
-
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 
+import android.content.ContentResolver;
+import android.provider.Settings.Secure;
+import android.os.Build;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -29,10 +30,22 @@ public class IdfaPlugin extends CordovaPlugin {
         Context context = this.cordova.getActivity().getApplicationContext();
         JSONObject result = new JSONObject();
 
+        boolean isAmazonDevice = Build.MANUFACTURER.equalsIgnoreCase("amazon");
+
         try {
-            AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
-            result.put("aaid", info.getId());
-            result.put("limitAdTracking", info.isLimitAdTrackingEnabled());
+            if( isAmazonDevice )
+            {
+                ContentResolver cr = this.cordova.getActivity().getApplicationContext().getContentResolver();
+                boolean limitAdTracking = (Secure.getInt(cr, "limit_ad_tracking") == 0) ? false : true;
+                String advertisingID = Secure.getString(cr, "advertising_id");
+                result.put("aaid", advertisingID);
+                result.put("limitAdTracking", limitAdTracking);
+            }
+            else {
+                AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
+                result.put("aaid", info.getId());
+                result.put("limitAdTracking", info.isLimitAdTrackingEnabled());
+            }
         } catch (Exception e) {
             Log.e(TAG, "getInfo failed", e);
         }
